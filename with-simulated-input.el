369ca9740267b6a53fb69fae71e1728acac4c031
Ryan C. Thompson <rct@thompsonclan.org>
2017-07-20 13:18:13 -0700

    Fix issues found by package-lint

--- with-simulated-input.el
+++ with-simulated-input.el
@@ -6,9 +6,9 @@
 ;; Author: Ryan C. Thompson
 ;; Created: Thu Jul 20 11:56:23 2017 (-0700)
 ;; Version: 1.0
-;; Package-Requires: ((emacs "24.4") (seq "0"))
+;; Package-Requires: ((emacs "24.4") (seq "2.0"))
 ;; URL: 
-;; Keywords: 
+;; Keywords: lisp, tools, extensions
 
 ;; This file is NOT part of GNU Emacs.
 
