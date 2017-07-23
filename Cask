4c68e02cc1fb0c45addd4ae103e6bd00dbcbd0a1
Ryan C. Thompson <rct@thompsonclan.org>
2017-07-22 19:51:17 -0700

    Allow evaluating lisp forms while simulating input

--- Cask
+++ Cask
@@ -7,6 +7,7 @@
          "A macro to simulate user input non-interactively.")
 
 (depends-on "seq")
+(depends-on "s")
 
 (development
  (depends-on "buttercup"
--- tests/test-with-simulated-input.el
+++ tests/test-with-simulated-input.el
@@ -4,7 +4,10 @@
 (require 'cl-lib)
 (require 'buttercup)
 
-(describe "Basic functionality of `with-simulated-input'"
+;; Needs to be dynamically bound
+(defvar mycollection)
+
+(describe "`with-simulated-input'"
   (it "should work for basic string input"
     (expect
      (with-simulated-input "hello RET"
@@ -35,32 +38,56 @@
        (list (read-string "First word: ")
              (read-string "Second word: ")))
      :to-equal '("hello" "world")))
-  (it "should not allow `C-g' in the input"
+  (it "should allow aborting via C-g in KEYS"
     (expect
-     (lambda ()
-       (with-simulated-input "C-g" (ignore)))
-     :to-throw)))
+     (condition-case nil
+         (with-simulated-input "C-g"
+           (read-string "Enter a string: "))
+       (quit 'caught-quit))
+     :to-be 'caught-quit))
 
-(describe "Using `with-simulated-input' with `completing-read'"
-  :var ((collection '("bluebird" "blueberry" "bluebell" "bluegrass" "baseball"))
-        (completing-read-function #'completing-read-default))
-  ;; Unambiguous completion
-  (it "should work with unambiguous tab completion"
-    (expect
-     ;; First TAB completes "blue", 2nd completes "bird"
-     (with-simulated-input "bl TAB bi TAB RET"
-       (completing-read "Choose: " collection))
-     :to-equal "bluebird"))
-  (it "should work with ambiguous tab completion"
-    (expect
-     (with-simulated-input "bl TAB C-j"
-       (completing-read "Choose: " collection))
-     :to-equal "blue"))
-  (it "should fail to exit with ambiguous completion and `require-match'"
-    (expect
-     (lambda ()
+
+  (describe "used with `completing-read'"
+    :var (collection completing-read-function)
+    (before-each
+      (setq mycollection '("bluebird" "blueberry" "bluebell" "bluegrass" "baseball")
+            completing-read-function #'completing-read-default))
+    ;; Unambiguous completion
+    (it "should work with unambiguous tab completion"
+      (expect
+       ;; First TAB completes "blue", 2nd completes "bird"
+       (with-simulated-input "bl TAB bi TAB RET"
+         (completing-read "Choose: " mycollection))
+       :to-equal "bluebird"))
+    (it "should work with ambiguous tab completion"
+      (expect
        (with-simulated-input "bl TAB C-j"
-         (completing-read "Choose: " collection nil t)))
-     :to-throw)))
+         (completing-read "Choose: " mycollection))
+       :to-equal "blue"))
+    (it "should fail to exit with ambiguous completion and `require-match'"
+      (expect
+       (lambda ()
+         (with-simulated-input "bl TAB C-j"
+           (completing-read "Choose: " mycollection nil t)))
+       :to-throw)))
+
+  (describe "using lisp forms in KEYS argument of `with-simulated-input'"
+    (it "should allow evaluating arbitrary lisp forms"
+      (expect
+       (with-simulated-input '("hello SPC" (insert "world") "RET")
+         (read-string "Enter a string: "))
+       :to-equal "hello world"))
+    (it "should allow lisp forms to throw errors"
+      (expect
+       (lambda ()
+         (with-simulated-input '("hello SPC" (error "Throwing an error") "RET")
+           (read-string "Enter a string: ")))
+       :to-throw))
+    (it "should not interpret lisp forms once BODY has finished"
+      (expect
+       (with-simulated-input '("hello SPC world RET RET"
+                               (error "Should not reach this error"))
+         (read-string "Enter a string: "))
+       :to-equal "hello world"))))
 
 ;;; test-with-simulated-input.el ends here
